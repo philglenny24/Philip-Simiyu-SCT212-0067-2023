3d601947fc63ffd8a625b6a51ecc1c2a4946344d
Philip Simiyu <glennytronephilip@gmail.com>
2025-05-14 23:33:14 +0300

    Update quiz1_2.c

--- Quizes/quiz1_2.c
+++ Quizes/quiz1_2.c
@@ -1,1 +1,41 @@
+#include <stdio.h>
+
+void flipArray(int*arr,int left,int right){
+    while (left<right){
+        int tmp=arr[left];
+        arr[left]=arr[right];
+        arr[right]=tmp;
+
+        left++;
+        right--;
+    }
+}
+void rotateArray(int*arr,int arr_len,int shift){
+    shift=shift%arr_len;
+
+    flipArray(arr,0,arr_len-1);
+    flipArray(arr,0,shift-1);
+    flipArray(arr,shift,arr_len-1);
+}
+int main(){
+    int test[]={23, 24, 25, 26, 27, 28};
+    int len=sizeof(test)/sizeof(test[0]);
+    int k=3;
+
+    printf("Before rotation:");
+    for(int i=0; i<len; i++){
+        printf("%d", test[i]);
+    }
+    printf("I\n");
+
+    rotateArray(test,len,k);
+
+    printf("After rotation:");
+    for(int i=0; i<len; i++){
+        printf("%d", test[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
 
