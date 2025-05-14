ec4b87599d9f8046efa1c5b7333329867e4afd1a
Philip Simiyu <glennytronephilip@gmail.com>
2025-05-14 23:34:04 +0300

    Update quiz1_3.py

--- Quizes/quiz1_3.py
+++ Quizes/quiz1_3.py
@@ -1,1 +1,13 @@
+def has_duplicate(numbers_list):
+    numbers_list.sort()
 
+    for i in range(len(numbers_list) - 1):
+        curr=numbers_list[i]
+        next=numbers_list[i+1]
+
+        if curr == next:
+            return True
+    return False
+
+print(has_duplicate([1,2,4,8,2,7,3]))
+print(has_duplicate([11,12,10,6,15]))
