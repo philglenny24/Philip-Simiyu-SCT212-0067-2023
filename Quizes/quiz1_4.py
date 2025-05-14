3edbffb95233dc0ab867970595e62ff4af6f9610
Philip Simiyu <glennytronephilip@gmail.com>
2025-05-14 23:35:22 +0300

    Update quiz1_4.py

--- Quizes/quiz1_4.py
+++ Quizes/quiz1_4.py
@@ -1,1 +1,10 @@
+def find_single_number(numbers):
+    running_xor=0
+    for current_number in numbers:
+        running_xor=running_xor^current_number
+    return running_xor
+
+test_Array=[4,1,2,1,2]
+result=find_single_number(test_Array)
+print(f"single number found: {result}")
 
